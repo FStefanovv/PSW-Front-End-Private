9ba26cfb56e28e46e752aa6d3d89ea3c2807591c
petkovic82 <petkovic.ra82.2019@uns.ac.rs>
2022-11-25 12:32:15 +0100

    annafinal

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { Routes, RouterModule } from "@angular/router";
 import { HomeComponent } from "./modules/pages/home/home.component";
 import { AppointmentsByDoctorComponent } from "./modules/hospital/appointments-by-doctor/appointments-by-doctor.component";
 import{RegisterBloodBankComponent} from "./modules/integration/register-blood-bank/register-blood-bank.component";
+import { ConfigurationComponent } from './modules/integration/configuration/configuration.component';
 const routes: Routes = [
   { path: '', redirectTo: '/appointments-by-doctor', pathMatch: 'full' },
   { path: 'appointments-by-doctor', component: AppointmentsByDoctorComponent},
   { path: 'home', component: HomeComponent },
-  {path:'register-blood-bank', component:RegisterBloodBankComponent}
+  {path:'register-blood-bank', component:RegisterBloodBankComponent},
+  {path:'configuration', component:ConfigurationComponent}
 ];
 
 @NgModule({
