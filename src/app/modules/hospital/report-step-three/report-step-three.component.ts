3fc3527dbab38dc0c5370101bb4ac4ff3f33e163
Milos Adnadjevic <adnadjevic.ra77.2019@uns.ac.rs>
2022-12-10 12:31:57 +0100

    step three

--- src/app/modules/hospital/report-step-three/report-step-three.component.ts
+++ src/app/modules/hospital/report-step-three/report-step-three.component.ts
@@ -1,9 +1,19 @@
 import { Component } from "@angular/core";
+import { ReportService } from "../services/report.service";
 
 @Component({
   selector: 'report-step-three',
   templateUrl: './report-step-three.component.html'
 })
 export class ReportStepThree{
+
+  listOfDrugs: string[]=[];
   
+  constructor(private reportService: ReportService) { }
+ 
+  ngOnInit(): void {
+
+  this.reportService.getDrugs().subscribe(res=>{this.listOfDrugs=res})
+
+  }
 }
